2bbecc46424877f041a198a434cee88a5bf85935
Semen Dutkin <s.dutkin@g.nsu.ru>
2025-02-03 22:17:11 +0700

    can identify types of value in string

---
+++ src/main/java/org/example/DataInterpriter.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class DataInterpriter {
+    enum Type {
+        INT,
+        FLOAT,
+        STRING
+    }
+
+    public static Type dataType(String string) {
+        if(string.matches("(-?[1-9][0-9]*)|0")) {
+            return Type.INT;
+        }
+        if(string.matches("-?(0\\.[0-9]+)|([1-9][0-9]*\\.[0-9]*)|(1\\.[0-9]+[eE][+-]?[0-9]+)")) {
+            return Type.FLOAT;
+        }
+        return Type.STRING;
+    }
+}

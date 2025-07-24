2b4fe56cef938b15e23b827d20c35bdf834e56a2
Semen Dutkin <s.dutkin@g.nsu.ru>
2025-07-24 18:24:18 +0700

    now compile regex

--- src/main/java/ru/shift/filterutility/service/DataInterpriter.java
+++ src/main/java/ru/shift/filterutility/service/DataInterpriter.java
@@ -2,12 +2,17 @@ package ru.shift.filterutility.service;
 
 import ru.shift.filterutility.model.DataType;
 
+import java.util.regex.Pattern;
+
 public class DataInterpriter {
+    private static final Pattern INT_PATTERN = Pattern.compile("([+-]?[1-9][0-9]*)|0");
+    private static final Pattern FLOAT_PATTERN = Pattern.compile("[+-]? ( (0\\.[0-9]+) | ([1-9][0-9]*\\.[0-9]+) | ([1-9][0-9]*\\.[0-9]+[eE][+-]?[0-9]+) )");
+
     public static DataType getType(String string) {
-        if(string.matches("(-?[1-9][0-9]*)|0")) {
+        if( INT_PATTERN.matcher(string).matches() ) {
             return DataType.INT;
         }
-        else if(string.matches("-?(0\\.[0-9]+)|([1-9][0-9]*\\.[0-9]+)|([1-9][0-9]*\\.[0-9]+[eE][+-]?[0-9]+)")) {
+        else if( FLOAT_PATTERN.matcher(string).matches() ) {
             return DataType.FLOAT;
         }
         else return DataType.STRING;

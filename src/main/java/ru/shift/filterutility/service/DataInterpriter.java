f4831813cb3fedf59311c4c13bb22440ea5c053e
Semen Dutkin <s.dutkin@g.nsu.ru>
2025-07-24 22:33:20 +0700

    fix float regex

--- src/main/java/ru/shift/filterutility/service/DataInterpriter.java
+++ src/main/java/ru/shift/filterutility/service/DataInterpriter.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 
 public class DataInterpriter {
     private static final Pattern INT_PATTERN = Pattern.compile("([+-]?[1-9][0-9]*)|0");
-    private static final Pattern FLOAT_PATTERN = Pattern.compile("[+-]? ( (0\\.[0-9]+) | ([1-9][0-9]*\\.[0-9]+) | ([1-9][0-9]*\\.[0-9]+[eE][+-]?[0-9]+) )");
+    private static final Pattern FLOAT_PATTERN = Pattern.compile("[+-]?((0\\.[0-9]+)|([1-9][0-9]*\\.[0-9]+)|([1-9][0-9]*\\.[0-9]+[eE][+-]?[0-9]+))");
 
     public static DataType getType(String string) {
         if( INT_PATTERN.matcher(string).matches() ) {

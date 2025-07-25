3af126e5c02f52f5c57b09be96e04ed189f053f5
Semen Dutkin <s.dutkin@g.nsu.ru>
2025-07-25 20:24:12 +0700

    allow leading zeroes in integer

--- src/main/java/ru/shift/filterutility/service/DataInterpriter.java
+++ src/main/java/ru/shift/filterutility/service/DataInterpriter.java
@@ -5,7 +5,7 @@ import ru.shift.filterutility.model.DataType;
 import java.util.regex.Pattern;
 
 public class DataInterpriter {
-    private static final Pattern INT_PATTERN = Pattern.compile("([+-]?[1-9][0-9]*)|0");
+    private static final Pattern INT_PATTERN = Pattern.compile("[+-]?[0-9]+");
     private static final Pattern FLOAT_PATTERN = Pattern.compile("[+-]?((0\\.[0-9]+)|([1-9][0-9]*\\.[0-9]+)|([1-9][0-9]*\\.[0-9]+[eE][+-]?[0-9]+))");
 
     public static DataType getType(String string) {
